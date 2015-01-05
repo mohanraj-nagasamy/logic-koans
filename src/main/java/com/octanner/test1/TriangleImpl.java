addee32394ff819e2ff2ca70608f63ab0675f319
Mohanraj Nagasamy <Mohanraj.Nagasamy@octanner.com>
2015-01-04 18:40:49 -0700

    implement triangles

--- src/main/java/com/octanner/test1/TriangleImpl.java
+++ src/main/java/com/octanner/test1/TriangleImpl.java
@@ -1,6 +1,9 @@
 package com.octanner.test1;
 
-public class TriangleImpl implements Triangle {
+import java.util.ArrayList;
+import java.util.List;
+
+/**/public class TriangleImpl implements Triangle {
     /**
      * Three segments of lengths A, B, C form a triangle iff
      * <p/>
@@ -20,6 +23,46 @@ public class TriangleImpl implements Triangle {
      */
     @Override
     public Integer[] getTriangleSides(Integer[] segments) {
-        return new Integer[]{};
+
+        List<Integer> list = new ArrayList<Integer>();
+
+        for (int i = 0; i < segments.length - 1; i++) {
+            int a = segments[i];
+            int b = segments[i + 1];
+
+            for (int j = (i + 2); j < (segments.length); j++) {
+                int c = segments[j];
+
+                if (isTriangle(a, b, c)) {
+                    list.add(a);
+                    list.add(b);
+                    list.add(c);
+                }
+            }
+        }
+
+
+//        for (int i = 0; i < segments.length; i++) {
+//            for (int j = (i + 1); j < (segments.length - 2); j++) {
+//                for (int k = (j + 1); k < (segments.length - 1); k++) {
+//                    int a = segments[i];
+//                    int b = segments[j];
+//                    int c = segments[k];
+//
+//                    if (isTriangle(a, b, c)) {
+//                        list.add(a);
+//                        list.add(b);
+//                        list.add(c);
+//                    }
+//                }
+//            }
+//        }
+
+        System.out.println("list = " + list);
+        return list.toArray(new Integer[list.size()]);
+    }
+
+    private static boolean isTriangle(int a, int b, int c) {
+        return ((a + b > c) && (b + c > a) && (a + c > b));
     }
 }
